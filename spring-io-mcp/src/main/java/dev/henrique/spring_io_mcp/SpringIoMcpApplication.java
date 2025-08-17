8470679779b3007a80b72d17e45e6726303a9043
Henrique da Silva <henriquec.dasilva@hotmail.com>
2025-08-17 15:03:19 -0300

    add new project mcp-io, in same repository

---
+++ spring-io-mcp/src/main/java/dev/henrique/spring_io_mcp/SpringIoMcpApplication.java
@@ -0,0 +1,13 @@
+package dev.henrique.spring_io_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIoMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIoMcpApplication.class, args);
+	}
+
+}
---
+++ spring-io-mcp/src/test/java/dev/henrique/spring_io_mcp/SpringIoMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.henrique.spring_io_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIoMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
